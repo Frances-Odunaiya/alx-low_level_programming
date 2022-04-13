1e68ee7532ee56a7a1618e57261304c0b56517fd
Rola01 <odunaiyafrances@gmail.com>
2022-04-13 07:25:18 -0700

    done

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+/**
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+printf("infinte loop incoming :(\n");
+
+i = 0;
+
+/*
+ * while (i < 10)
+ * {
+ * infinte loop - No increase of variable
+ * putchar(i);
+}
+*/
+
+printf("infinte loop avoided! \\o/\n");
+return (0);
+}

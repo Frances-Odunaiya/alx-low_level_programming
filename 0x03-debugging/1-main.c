8ff6546508881d0b3accecb0aa91aa2a3d9886cd
Rola01 <odunaiyafrances@gmail.com>
2022-04-13 07:42:49 -0700

    done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,16 @@ int i;
 printf("infinite loop incoming :(\n");
 
 i = 0;
+
+
 /*
  * while (i < 10)
  * {
  * infinte loop - No increase of variable
  * putchar(i);
- * }
- */
+}
+*/
+
 printf("infinite loop avoided! \\o/\n");
 return (0);
 }

6a69542c34ca4ff9c0de9ff7b8039d1b3cd8acc0
Rola01 <odunaiyafrances@gmail.com>
2022-04-25 12:18:29 -0700

    do

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 {
 	int i;
 	int j;
-	char c[] = {44, 59,46,33,63,34,40,41,123,125,32,10,9};
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	i = 0;
 
@@ -23,7 +23,7 @@ char *cap_string(char *str)
 		while (c[j] != '\0')
 		{
 			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-			{ 
+			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 	

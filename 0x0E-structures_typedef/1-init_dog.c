245558029d0c46d3bb8e8f696783b0564f4e02e4
Rola01 <odunaiyafrances@gmail.com>
2022-05-12 22:46:36 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
  * Return: nothing
  **/
 
-void init_dog(struct dog *d, chat *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;

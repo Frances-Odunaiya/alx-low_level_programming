92e1cbad26cafe43a675364b913426af276379fb
Rola01 <odunaiyafrances@gmail.com>
2022-05-13 14:40:58 -0700

    done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+/**
+ * op_add - Adds two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+ **/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+ **/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modules two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+ **/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

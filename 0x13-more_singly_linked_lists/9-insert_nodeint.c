69464aa412dfb7a4c287d516bc1111f5afcdcea4
Rola01 <odunaiyafrances@gmail.com>
2022-06-05 02:39:43 -0700

    final changes made

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,13 +35,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-
+	
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 	}
-
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
+	
 	return (new);
 }
-

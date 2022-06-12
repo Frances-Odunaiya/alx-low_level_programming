0acf4c5645d67a9db28a2b1c840d3e46b7fbdc8d
Rola01 <odunaiyafrances@gmail.com>
2022-06-12 10:17:33 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		tet_content = "";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
